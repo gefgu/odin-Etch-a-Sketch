5e347328c88f503eef6e1ffe9037cf7340bc32ca
gefgu <gefgu@hotmail.com>
2022-01-15 19:15:06 -0300

    Add pseudocode for js functions

---
+++ script.js
@@ -0,0 +1,14 @@
+// Add variable for div.container
+// For loop - loop 16 times
+// Nested For loop -loop 16 times
+// Inside the nested for loop create tile and append to container
+function createGrid() {
+
+}
+
+// create div element
+// add class "tile"
+// return div element with class
+function createTile() {
+
+}
